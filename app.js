4d128e86dd51a95d0fecd35cd2e91441912e9c91
balamahi077 <bk6417182@gmail.com>
2026-09-13 14:40:48 +0530

    Updated API URL to production server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://pg-backend-8fi9.onrender.com/api';
 
 // Function to fetch and display rooms
 async function loadRooms() {
